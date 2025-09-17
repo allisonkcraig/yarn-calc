7885e81b8a8c591253c20fb565f0fd801dc82a0c
Allison  Craig <allisoncraig@mac.lan>
2025-09-16 22:24:11 -0400

    stub out future features and add gitignore

---
+++ src/GaugeCalc.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class GaugeCalc extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return "Gauge Calc coming soon"
+  }
+}
+
+export default GaugeCalc; 
---
+++ src/YardageCalc.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class YardageCalc extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return "Yardage Calc coming soon"
+  }
+}
+
+export default YardageCalc; 
