0fe75ae0276a591a4ee6eea6bc40fab798cb3735
Allison  Craig <allisoncraig@mac.lan>
2025-09-17 21:04:08 -0400

    add font to header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import GaugeCalc from './GaugeCalc';
 function App() {
   return (
     <div className="App">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=New+Rocker&display=swap" rel="stylesheet"></link>
+      </head>
       <header className="App-header">
         <h1>Helpful Knitting and Crochet Calculators</h1>
       </header>
