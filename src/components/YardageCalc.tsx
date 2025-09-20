a3cc7aaa646e5b43c4283e40c9653916c9300409
Allison  Craig <allisoncraig@mac.lan>
2025-09-19 20:22:28 -0400

    style and change button color to a11y blue

--- src/components/YardageCalc.tsx
+++ src/components/YardageCalc.tsx
@@ -43,63 +43,67 @@ const YardageCalc: React.FC = () => {
   }
 
   const yardageCalcForm = (
-    <form onSubmit={handleFormSubmit}>
-      <label htmlFor="suggestedYarnWeight">
-        Suggested Yarn Weight per Skein:
-      </label>
-      <input
-        type="number"
-        id="suggestedYarnWeight"
-        value={suggestedYarnWeight}
-        onChange={(e) => setSuggestedYarnWeight(Number(e.target.value))}
-      />
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="suggestedYarnWeight">
+          Suggested Yarn Weight per Skein:
+        </label>
+        <input
+          type="number"
+          id="suggestedYarnWeight"
+          value={suggestedYarnWeight}
+          onChange={(e) => setSuggestedYarnWeight(Number(e.target.value))}
+        />
+        <br />
+        <label htmlFor="suggestedYarnYardage">
+          Suggested Meters per Skein:
+        </label>
+        <input
+          type="number"
+          id="suggestedYarnYardage"
+          value={suggestedYarnYardage}
+          onChange={(e) => setSuggestedYarnYardage(Number(e.target.value))}
+        />
+        <br />
+        <label htmlFor="patternSuggestedWeight">Grams needed:</label>
+        <input
+          type="number"
+          id="patternSuggestedWeight"
+          value={patternSuggestedWeight}
+          onChange={(e) => setPatternSuggestedWeight(Number(e.target.value))}
+        />
+        <br />
+        <label htmlFor="chosenYarnYardage">Chosen Yarn Yardage:</label>
+        <input
+          type="number"
+          id="chosenYarnYardage"
+          value={chosenYarnYardage}
+          onChange={(e) => setChosenYarnYardage(Number(e.target.value))}
+          required
+        />
+        <br />
+        <label htmlFor="includeWastage">Include 10% wastage?:</label>
+        <input
+          type="checkbox"
+          id="includeWastage"
+          name="includeWastage"
+          checked
+          onChange={(e) => setIncludeWastage(e.target.checked)}
+        />
+        <br />
+        <button className="button" type="submit">
+          Get Value
+        </button>
+      </form>
       <br />
-      <label htmlFor="suggestedYarnYardage">Suggested Meters per Skein:</label>
-      <input
-        type="number"
-        id="suggestedYarnYardage"
-        value={suggestedYarnYardage}
-        onChange={(e) => setSuggestedYarnYardage(Number(e.target.value))}
-      />
+      <h3>Skeins needed rounded up: {Math.round(skeinCount)}</h3>
       <br />
-      <label htmlFor="patternSuggestedWeight">Grams needed:</label>
-      <input
-        type="number"
-        id="patternSuggestedWeight"
-        value={patternSuggestedWeight}
-        onChange={(e) => setPatternSuggestedWeight(Number(e.target.value))}
-      />
-      <br />
-      <label htmlFor="chosenYarnYardage">Chosen Yarn Yardage:</label>
-      <input
-        type="number"
-        id="chosenYarnYardage"
-        value={chosenYarnYardage}
-        onChange={(e) => setChosenYarnYardage(Number(e.target.value))}
-        required
-      />
-      <br />
-      <label htmlFor="includeWastage">Include 10% wastage?:</label>
-      <input
-        type="checkbox"
-        id="includeWastage"
-        name="includeWastage"
-        checked
-        onChange={(e) => setIncludeWastage(e.target.checked)}
-      />
-      <br />
-      <button className="button" type="submit">
-        Get Value
-      </button>
-      <br />
-      <span>Skeins needed rounded up: {Math.round(skeinCount)}</span>
-      <br />
-      <span>Skeins needed unrounded: {skeinCount.toFixed(2)}</span>
-    </form>
+      <h3>Skeins needed unrounded: {skeinCount.toFixed(2)}</h3>
+    </div>
   );
 
   return (
-    <div className="Calc-container">
+    <div>
       <Calculator
         title="Calculate Number of Skeins Needed"
         calcForm={yardageCalcForm}
